02a653a97e592f494bab0ce3abfb6e7fd9449c52
Shai Axelrod <saxelrod@g.jct.ac.il>
2023-08-06 12:39:26 +0300

    edit filter_tf_photos script

--- scripts/filter_tf_photos.py
+++ scripts/filter_tf_photos.py
@@ -1,20 +1,23 @@
 import os
 import json
-import shutil
+from pathlib import Path
+import pandas as pd
 
+original_csv_path = os.path.join('..', 'data', 'tfls.csv')
+filtered_csv_path = os.path.join('..', 'data', 'filtered_tfls.csv')
+df = pd.read_csv(original_csv_path)
+filtered_df = pd.DataFrame(columns=df.columns)
+print(pd. __version__)
 
-photo_dir =os.path.join('..', 'photos')
-tl_photo_dir  = os.path.join('..', 'tl_photos')
+tl_label = 'traffic light'
+filtered_idx = 0
+for index, row in df.iterrows():
+    gt_data = json.loads(Path(os.path.join('..', 'data',row['json_path'])).read_text())
+    objects = [o for o in gt_data['objects'] if o['label'] == tl_label]
+    if len(objects):
+        # Add the row to the filtered DataFrame if it satisfies the condition
+        filtered_df.loc[filtered_idx] = row
+        filtered_idx += 1
 
-if not os.path.exists(tl_photo_dir):
-    os.makedirs(tl_photo_dir)
-
-for filename in os.listdir(photo_dir):
-    if filename.endswith(".png"):
-        json_file_path = os.path.join(photo_dir, filename.replace('_leftImg8bit.png', '_gtFine_polygons.json'))
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-            for obj in data["objects"]:
-                if obj["label"] == "traffic light":
-                    shutil.move(os.path.join(photo_dir, filename), os.path.join(tl_photo_dir, filename))
-                    break
+# Save the new CSV with filtered rows
+filtered_df.to_csv(filtered_csv_path, index=False)
